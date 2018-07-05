26a2db9d3880492a934085719ca7ac5fd0d0fbaf
Zeargg <30478734+Zeargg@users.noreply.github.com>
2018-07-05 20:30:12 +0500

    2 1 HW1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Meal {
     private final LocalDateTime dateTime;
@@ -30,5 +31,9 @@ public class Meal {
 
     public LocalDate getDate(){
         return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime(){
+        return dateTime.toLocalTime();
     }
 }
