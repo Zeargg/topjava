c06cfb51ee9d9f74bccb3da069247b19281f5589
Zeargg <30478734+Zeargg@users.noreply.github.com>
2018-07-17 16:35:23 +0500

    3 10 db implementation

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  id               INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name             VARCHAR(50)                 NOT NULL,
+  email            VARCHAR(50)                 NOT NULL,
+  password         VARCHAR(50)                 NOT NULL,
+  registered       TIMESTAMP DEFAULT now() NOT NULL,
+  enabled          BOOL DEFAULT TRUE       NOT NULL,
+  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+);
+ALTER TABLE users AUTO_INCREMENT = 100000;
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR(20),
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
